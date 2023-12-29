3d0d3dd63598626ebf35b45fcae9adbabac8e1eb
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-12-29 14:44:16 -0500

    added home and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Routes>
-        <Route path='' element={""} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
