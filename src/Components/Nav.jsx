ddff3722939d70ef48d593b5558fafd0584862ec
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-12-29 15:20:19 -0500

    updating the navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+ import logo from '../assets/JohnOliverFitness.png'
 
 const Nav = () => {
     return (
         <nav>
-            NavBar
+            <img src={logo}  alt='John Oliver Logo'/>
+
+            <h2>Training</h2>
+            <h2>About</h2>
         </nav>
     );
 };
